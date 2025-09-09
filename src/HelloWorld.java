1451b337f971149ed1e10219109e4ba2618e5b41
Jannis Lauer <j.lauer@elio-systems.com>
2025-09-09 12:00:24 +0200

    Add Variable class and simplify HelloWorld output

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,20 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        // Zeichenkette
-        var name = "Jannis";
-        //String name = "Jannis";
-
-        // Gibt in Console aus
-        System.out.print("Hello, " + name + "\n");
-
-        // Weißt automatisch zu
-        var gesamtalter = 5;
-        //int gesamtalter = 5;
-
-        // Wird in double converiter dann geteilt und gesetzt
-        var durchschnitt = (double) gesamtalter/2;
-        // double durchschnitt = (double) gesamtalter/2;
-
-        System.out.println(gesamtalter + " " + durchschnitt);
+        System.out.println("Hello World!");
     }
 }
