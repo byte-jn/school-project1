a1f31ccbe4f79a55fdfdec5f2acdf8b1642c931a
Jannis Lauer <j.lauer@elio-systems.com>
2025-09-09 11:33:34 +0200

    Refactor project structure and update HelloWorld for enhanced output

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,19 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.print("Hello World! \n");
-        System.out.println("Hello World!");
+        // Zeichenkette
+        var name = "Jannis";
+        //String name = "Jannis";
+
+        // Gibt in Console aus
+        System.out.print("Hello, " + name + "\n");
+
+        // Weißt automatisch zu
+        var gesamtalter = 40;
+        //int gesamtalter = 40;
+
+        // Wird geteilt und gesetzt
+        double durchschnitt = 40/3;
+
+        System.out.println(gesamtalter + " " + durchschnitt);
     }
 }
