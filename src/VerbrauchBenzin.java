597522821e62223e095b73bf84ccc21c2a12b889
Jannis Lauer <j.lauer@elio-systems.com>
2025-09-16 11:23:03 +0200

    Add input validation for Strecke in VerbrauchBenzin class and improve output formatting

--- src/VerbrauchBenzin.java
+++ src/VerbrauchBenzin.java
@@ -5,12 +5,19 @@ public class VerbrauchBenzin {
         Scanner input = new Scanner(System.in); // Scanner Objekt erzeugt
         System.out.print("Gib die Strecke ein:"); // Info in der Konsole ausgeben
         double strecke = input.nextDouble(); // Strecke definieren
-        System.out.print("Gib die verbrauchten Liter ein:"); // Info in der Konsole ausgeben
-        double literBenzin = input.nextDouble();
 
-        System.out.print("\n");
+        if (strecke == 0) {
+            System.out.print("Du bist zu wenig gefahren");
+        } else {
+            System.out.print("Gib die verbrauchten Liter ein:"); // Info in der Konsole ausgeben
+            double literBenzin = input.nextDouble();
 
-        double verbrauch = literBenzin * 100 / strecke;
-        System.out.print("Dein Verbrauch liegt bei " + verbrauch + " l/100km");
+            System.out.print("\n");
+
+            if (strecke < 0) strecke *= -1;
+            double verbrauch = literBenzin * 100 / strecke;
+            verbrauch = ((double) Math.round(verbrauch * 100)) / 100;
+            System.out.print("Dein Verbrauch liegt bei " + verbrauch + " l/100km");
+        }
     }
 }
