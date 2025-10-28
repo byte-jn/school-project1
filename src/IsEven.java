450a054f53a9ee92ea0c9079f2298ce03e8f6556
jannis <jannis280@outlook.de>
2025-10-28 15:28:56 +0100

    Aufgaben zum PIN-Code, Versandkostenrechner, Rabattrechner, Gerade Zahl erkennen. Vom 28.10.25

--- src/IsEven.java
+++ src/IsEven.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class IsNumEven {
+public class IsEven {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Gib die Zahl ein:");
@@ -8,14 +8,14 @@ public class IsNumEven {
 
         boolean isEven = ((num % 2) == 0);
 
-        System.out.println(isEven);
+        System.out.println(isEven); // ture / false
 
         if (isEven) {
-            num *= 2;
+            num *= 2; // if even *2
         } else {
-            num += 1;
+            num += 1; // if not even +1
         }
 
-        System.out.println(num); // true / false
+        System.out.println(num); // Wert
     }
 }
