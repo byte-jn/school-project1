2fa5f507111f6803086a96dad3a39169e7510da3
Jannis Lauer <j.lauer@elio-systems.com>
2025-11-05 16:57:12 +0100

    c) Neben dem RGB Farbmodell soll es die Möglichkeit geben auch weitere Farbmodelle auszuwählen    (z.B. CMYK, Graustufenbild, schwarz-weiß).

--- src/Bild.java
+++ src/Bild.java
@@ -10,17 +10,30 @@ public class Bild {
         System.out.print("Gib die gewünschten Beite ein:");
         double b = input.nextInt();
 
-        System.out.print("Was für eine Farbvielfalt hat das Bild? Graustufen (sw), Farbbild (f):");
+        System.out.print("Was für eine Farbvielfalt hat das Bild? Schwarzweißbild (sw), Graustufen (g), Farbbild (f), CMYK (c):");
         String colorType = input.next();
-        double bytePix = 3; // standard
-        if (colorType == "sw") {
-            bytePix = 2;
+
+        double bytePix;
+        switch (colorType){
+            case "sw":
+                bytePix = 1; // Schwarzweißbild
+                break;
+            case "g":
+                bytePix = 2; // Graustufen
+                break;
+            case "c":
+                bytePix = 4; // Farbbild
+                break;
+            default:
+                bytePix = 3; // CMYK
+                break;
         }
-        System.out.println("Dass sind dann " + bytePix + "bit pro Pixel");
+
+        System.out.println("Dass sind dann " + bytePix + " bit pro Pixel");
 
         System.out.print("Anzahl Bilder:");
         double anzahlBilder = input.nextInt();
 
-        System.out.println(((double) Math.round(h*b*bytePix*anzahlBilder/1024/1024/1024*100))/100);
+        System.out.println("Ihre Sammlung ist " + (((double) Math.round(h*b*bytePix*anzahlBilder/1024/1024/1024*100))/100) + "GB groß");
     }
 }
