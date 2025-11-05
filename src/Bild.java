6d29c44d8a8319d6e1f797ac85721454dde43302
Jannis Lauer <j.lauer@elio-systems.com>
2025-11-05 16:43:59 +0100

    b) Die Benutzerin soll eingeben, ob es sich um Farbbilder (RGB) ist oder Graustufenbilder (8-bit grayscale) handelt.

--- src/Bild.java
+++ src/Bild.java
@@ -10,14 +10,17 @@ public class Bild {
         System.out.print("Gib die gewünschten Beite ein:");
         double b = input.nextInt();
 
-        System.out.print("Bite pro Pixel:");
-        double bytePix = input.nextInt();
+        System.out.print("Was für eine Farbvielfalt hat das Bild? Graustufen (sw), Farbbild (f):");
+        String colorType = input.next();
+        double bytePix = 3; // standard
+        if (colorType == "sw") {
+            bytePix = 2;
+        }
+        System.out.println("Dass sind dann " + bytePix + "bit pro Pixel");
 
         System.out.print("Anzahl Bilder:");
         double anzahlBilder = input.nextInt();
 
         System.out.println(((double) Math.round(h*b*bytePix*anzahlBilder/1024/1024/1024*100))/100);
-
-
     }
 }
