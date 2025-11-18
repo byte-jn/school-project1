95fd1121724f2fe87fd6d7c43e86ab5cb20b780b
Jannis Lauer <j.lauer@elio-systems.com>
2025-11-18 10:56:06 +0100

    Add validation for month number input in NumberToMonth class

--- src/Verzweigungen_2/NumberToMonth.java
+++ src/Verzweigungen_2/NumberToMonth.java
@@ -6,8 +6,17 @@ public class NumberToMonth {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Gib eine Zahl von 1 bis 12 ein:");
+        validateMonthNumber(input.nextInt());
         System.out.println(numberToMonth(input.nextInt()));
     }
+
+    public static void validateMonthNumber(int number) {
+        if (number < 1 || number > 12) {
+            System.out.println("Fehler: Invaild month number");
+            System.exit(1);
+        }
+    }
+
     public static String numberToMonth(int number) {
         return switch (number) {
             case 1 -> "Januar";
@@ -22,7 +31,7 @@ public class NumberToMonth {
             case 10 -> "Oktober";
             case 11 -> "November";
             case 12 -> "Dezember";
-            default -> "Ungültige Zahl";
+            default -> "Offset Error";
         };
     }
 }
