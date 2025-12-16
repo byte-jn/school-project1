04be6f92402d66c68656af64140fe17ea2b5a332
Jannis Lauer <j.lauer@elio-systems.com>
2025-12-16 11:27:19 +0100

    Refactor Weihnachtsbaum class to improve tree and trunk printing logic

--- src/_4_Schleifen/Weihnachtsbaum.java
+++ src/_4_Schleifen/Weihnachtsbaum.java
@@ -5,15 +5,16 @@ import java.util.Scanner;
 public class Weihnachtsbaum {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
         System.out.print("Bitte geben Sie die Breite des Weihnachtsbaums ein: ");
-        int breite = (scanner.nextInt()+1)/2;
+        int breite = isEvenReturnOdd(scanner.nextInt()/2 + 1) ;
 
-        System.out.print("Bitte geben Sie die Breite des Stamms ein: ");
-        int stammbreite = scanner.nextInt();
+        printTree(breite);
 
-        System.out.print("Bitte geben Sie die Höhe des Stamms ein: ");
-        int stammhoehe = scanner.nextInt();
+        printStamm(breite);
+    }
 
+    public static void printTree(int breite){
         for (int i = 1; i <= breite; i++) {
             // Leerzeichen ausgeben
             for (int j = breite; j > i; j--) {
@@ -26,18 +27,27 @@ public class Weihnachtsbaum {
             // Neue Zeile
             System.out.println();
         }
+    }
 
-        for (int i = 1; i <= stammhoehe - 1; i++) {
+    public static void printStamm(int breite) {
+        int stammbreite = isEvenReturnOdd(breite / 2);
+        int stammhoehe = isEvenReturnOdd(breite / 2 - 1);
+
+        for (int i = 0; i <= stammhoehe - 1; i++) {
             // Leerzeichen ausgeben
             for (int j = 1; j <= breite - (stammbreite / 2) - 1; j++) {
                 System.out.print(" ");
             }
             // Stamm ausgeben
             for (int k = 1; k <= stammbreite; k++) {
-                System.out.print("*");
+                System.out.print("I");
             }
             // Neue Zeile
             System.out.println();
         }
+    }
+
+    public static int isEvenReturnOdd(int number) {
+        return number % 2 == 0 ? number + 1 : number;
     }
 }
